9abbbb499c21950f255497115f420733fcaa0619
11jl98 <joaoferreira981011@gmail.com>
2021-06-02 16:58:12 -0300

    commit

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -6,6 +6,18 @@ const produtosModel = require('../models/produtosModel')
 router.get('/', async (req, res) => {
     return res.render('produtos');
 });
- router.get('/', async(req, res)=>{
-     
- })
+router.get('/read', async (req, res) => {
+    try {
+        const result = await produtosModel.query('select * from cliente;')
+        return res.status(200).json(result)
+    } catch (error) {
+        if (error.errno === 1064) {
+            return res.status(400).send(error)
+        }
+
+        return res.status(500).send(error)
+    }
+})
+router.get('/read/:id',async(req, res)=>{
+    
+})
